386ffb284368aa560d832209567e3691013527d2
philippe <philoufilou2@gmail.com>
2020-12-17 11:01:18 +0100

    ajout du nom de la catégorie dans la liste data_category

--- scrapcategory.py
+++ scrapcategory.py
@@ -8,7 +8,8 @@ import os
 
 
 def scrap_category(url_category: str) -> list:
-    """ retourne toutes les données des livres d'une catérorie """
+    """ retourne la liste de toutes les données des livres de la catérorie
+    dont l'url est entrée en paramètre """
 
     next_page = True
     url_category_page = url_category
@@ -19,22 +20,17 @@ def scrap_category(url_category: str) -> list:
         response.encoding = 'utf-8'
         soup = BeautifulSoup(response.text, "lxml")
 
+        if url_category_page == url_category:
+            data_category.append(soup.find('h1').text)  # enregistre le nom de la catégorie
+
         for h3 in soup.findAll('h3'):  # balise 'h3' contient les urls des livres
             url_book = h3.find('a')['href'].replace('../../..', csts.URL_SITE + '/catalogue')
             data_category.append(scrapbook.scrap_book(url_book))
 
         next_page = soup.find('li', {"class": "next"})
         if next_page:
-            link_new_page = soup.find('li', {"class": "next"}).find('a')['href']
-            url_category_page = url_category.replace('index.html', '') + link_new_page
+            url_category_page = url_category.replace('index.html', '') + soup.find('li', {"class": "next"}).find('a')['href']
     return data_category
-
-
-def category_name(url_category: str) -> str:
-    """ retourne le nom de la catégorie """
-    response = utils.requests_error(url_category)
-    response.encoding = 'utf-8'
-    return BeautifulSoup(response.text, "lxml").find('h1').text
 
 
 def main():
@@ -46,10 +42,10 @@ def main():
                         help="taper '-s' ou '--save' pour sauvegarder les données d'une catégorie")
     args = parser.parse_args()
     data_category = scrap_category(args.url_category)
-
+   
     if args.save:
         os.makedirs(csts.PATH_CAT, exist_ok=True)
-        utils.record_csv(csts.PATH_CAT, category_name(args.url_category), data_category)
+        utils.record_csv(csts.PATH_CAT, data_category[0], data_category[1:])
     else:
         print(data_category)
 
